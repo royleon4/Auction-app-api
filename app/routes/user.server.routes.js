6a15d3a5766ce0e34d1dda18665317a227697ac9
coscc770 <ash.03@hotmail.com>
2018-03-09 14:45:42 +1300

    test deployment - with users but no routes

--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -2,16 +2,16 @@ const users = require('../controllers/user.server.controllers'),
       auth = require('../lib/middleware');
 
 module.exports = function(app){
-  app.route('/api/v1/users')
-    .post(users.create);
-
-  app.route('/api/v1/users/:id')
-    .get(users.get_one)
-    .patch(auth.isAuthenticated, users.update);
-
-  app.route('/api/v1/users/login')
-    .post(users.login);
-
-  app.route('/api/v1/users/logout')
-    .post(auth.isAuthenticated, users.logout);
+  // app.route('/api/v1/users')
+  //   .post(users.create);
+  //
+  // app.route('/api/v1/users/:id')
+  //   .get(users.get_one)
+  //   .patch(auth.isAuthenticated, users.update);
+  //
+  // app.route('/api/v1/users/login')
+  //   .post(users.login);
+  //
+  // app.route('/api/v1/users/logout')
+  //   .post(auth.isAuthenticated, users.logout);
 }
