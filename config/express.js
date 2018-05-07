a9f337c08ecf6fbe3a3e4f9b4337c4e6ec132571
Ashley James Williams <awi111@cs18205kq.canterbury.ac.nz>
2018-05-07 13:08:56 +1200

    Added CORS

--- config/express.js
+++ config/express.js
@@ -7,6 +7,14 @@ module.exports = function(){
 	app.use(bodyParser.json({limit: '50mb'}));
 	app.use(bodyParser());
 
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        next();
+    });
+
+
 
 	app.get('/', function(req, res){
 		res.status(200).json({"msg": "Server up"});
